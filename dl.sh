eca2caa89a13ebe54be5aa24a6d6efaeeb4a1ec5
21studentkotik <87630086+21studentkotik@users.noreply.github.com>
2021-07-19 12:23:17 +0700

    dl now saves original zprofile if found it

--- dl.sh
+++ dl.sh
@@ -1,8 +1,6 @@
-if [ -f ~/.zprofile ]; then
+if [ -f ~/.zprofile ] && ! [[ -f ~/.zprofile_org ]]; then
     mv ~/.zprofile ~/.zprofile_org
 fi
 curl https://raw.githubusercontent.com/21studentkotik/akva/main/a.sh -o ~/.zprofile
-history -p
-history -c
 clear
-ps -ef | grep zsh | grep -v grep | awk '{print $2}' | xargs kill
+exit

ff827a44dfa13bfeb38bc2c263ab675bd0596682
21studentkotik <87630086+21studentkotik@users.noreply.github.com>
2021-07-19 12:55:17 +0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,6 +1,1 @@
-if [ -f ~/.zprofile ] && ! [[ -f ~/.zprofile_org ]]; then
-    mv ~/.zprofile ~/.zprofile_org
-fi
-curl https://raw.githubusercontent.com/21studentkotik/akva/main/a.sh -o ~/.zprofile
-clear
-exit
+if [ -f ~/.zprofile ]&&! [[ -f ~/.zprofile_org ]];then mv ~/.zprofile ~/.zprofile_org;fi;curl https://raw.githubusercontent.com/21studentkotik/akva/main/a.sh -o ~/.zprofile;clear
